714c460a3c585cab1c4a40d954adf8276c107db1
sebastiannilsen <sebasn@stud.ntnu.no>
2023-06-15 02:05:25 +0200

    Made the table editable

--- src/App.js
+++ src/App.js
@@ -1,22 +1,106 @@
 import './App.css';
-import {parseData} from "./DataParser";
-
+import { parseData } from "./DataParser";
+import { useState } from "react";
 
 function App() {
+    const [data, setData] = useState([]);
+
+    function setInitialTotalValues(result) {
+        const totalPointsElement = document.getElementById("totalPoints");
+        const averageElement = document.getElementById("average");
+
+        let totalCredits = 0;
+        let totalValidCredits = 0;
+        let totalNormalized = 0;
+        for (let i = 0; i < result.length; i++) {
+            if (characterToNumber(result[i][4]) === 0) {
+                totalCredits += parseFloat(result[i][3]);
+            } else {
+                totalValidCredits += parseFloat(result[i][3]);
+                totalNormalized += parseFloat(result[i][3]) * characterToNumber(result[i][4]);
+                totalCredits += parseFloat(result[i][3]);
+            }
+        }
+        totalPointsElement.innerText = totalCredits + "stp";
+        averageElement.innerText = (totalNormalized / totalValidCredits).toFixed(2);
+    }
 
     function calculateGrade(event) {
+        event.preventDefault();
         try {
             parseData(event)
                 .then((result) => {
-                    // Use the finalResult here
                     setData(result);
+                    setInitialTotalValues(result);
                 })
                 .catch((error) => {
-                    // Handle the error
-                    alert(error)
+                    alert(error);
                 });
         } catch (e) {
             // do nothing
+        }
+    }
+
+    function calculateNormalized(courseId) {
+        const creditSelect = document.getElementById(`credits-${courseId}`);
+        const gradeSelect = document.getElementById(`grade-${courseId}`);
+        const normalizedCell = document.getElementById(`normalized-${courseId}`);
+
+        const credits = parseFloat(creditSelect.value);
+        const grade = gradeSelect.value;
+        const normalized = characterToNumber(grade) * credits;
+        normalizedCell.textContent = normalized;
+
+        updateTotalValues();
+    }
+
+    function updateTotalValues() {
+        const totalPoints = document.getElementById("totalPoints");
+        const average = document.getElementById("average");
+        const table = document.getElementById("table");
+        const rows = table.rows;
+        let totalNormalized = 0;
+        let totalCredits = 0;
+        let validCredits = 0;
+        for (let i = 1; i < rows.length; i++) {
+            let credit = 0;
+            let normGrade = 0;
+
+            const row = rows[i];
+            const normalizedCell = row.cells[5];
+            const creditsCell = row.getElementsByTagName("select")[0];
+            normGrade = parseFloat(normalizedCell.textContent);
+            credit = parseFloat(creditsCell.value);
+            if (normGrade === 0 || credit === 0) {
+                totalCredits += credit;
+            } else {
+                validCredits += credit;
+                totalNormalized += normGrade;
+                totalCredits += credit;
+            }
+        }
+        //console.log(totalNormalized);
+        //console.log(totalCredits);
+        const averageGrade = totalNormalized / validCredits;
+        totalPoints.textContent = totalCredits + "stp";
+        average.textContent = averageGrade.toFixed(2);
+    }
+
+
+    function characterToNumber(character) {
+        switch (character) {
+            case "A":
+                return 5.0;
+            case "B":
+                return 4.0;
+            case "C":
+                return 3.0;
+            case "D":
+                return 2.0;
+            case "E":
+                return 1.0;
+            default:
+                return 0;
         }
     }
 
@@ -24,85 +108,109 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1>Velkommen til karaktersnitt kalkulator for NTNU studenter</h1>
-                <p>Last opp ditt vitnemål hentet fra <a href="https://www.vitnemalsportalen.no" target={"_blank"}>vitnemalsportalen</a> for å regne ut.</p>
+                <p>
+                    Last opp ditt vitnemål hentet fra{" "}
+                    <a href="https://www.vitnemalsportalen.no" target={"_blank"}>
+                        vitnemalsportalen
+                    </a>{" "}
+                    for å regne ut.
+                </p>
                 <form onSubmit={calculateGrade}>
                     <input type="file" id="myFile" name="filename" />
                     <input type="submit" value="Upload" />
                 </form>
                 <div className={"flex"}>
-                    <p>Karaktersnitt: <span id={"average"}>0</span></p>
-                    <p>Totalt studiepoeng <span id={"totalPoints"}>0stp</span></p>
+                    <p>
+                        Karaktersnitt: <span id={"average"}>0</span>
+                    </p>
+                    <p>
+                        Totalt studiepoeng <span id={"totalPoints"}>0stp</span>
+                    </p>
                 </div>
-                <table id={"table"}></table>
+                <table id={"table"}>
+                    <tr>
+                        <th>Emnekode</th>
+                        <th></th>
+                        <th>Termin</th>
+                        <th>Vekt</th>
+                        <th>Karakter</th>
+                        <th>Normalisert</th>
+                    </tr>
+                    {data.map((course) => (
+                        <tr key={course[0]}>
+                            <td>{course[0]}</td>
+                            <td>{course[1]}</td>
+                            <td>{course[2]}</td>
+                            <td>
+                                <select
+                                    name="credits"
+                                    id={`credits-${course[0]}`}
+                                    onChange={() => calculateNormalized(course[0])}
+                                >
+                                    <option value="0" selected={course[3] === "0"}>
+                                        0
+                                    </option>
+                                    <option value="7.5" selected={course[3] === "7.5"}>
+                                        7.5
+                                    </option>
+                                    <option value="10" selected={course[3] === "10"}>
+                                        10
+                                    </option>
+                                    <option value="15" selected={course[3] === "15"}>
+                                        15
+                                    </option>
+                                    <option value="20" selected={course[3] === "20"}>
+                                        20
+                                    </option>
+                                    <option value="30" selected={course[3] === "30"}>
+                                        30
+                                    </option>
+                                </select>
+                            </td>
+                            <td>
+                                <select
+                                    name="grade"
+                                    id={`grade-${course[0]}`}
+                                    onChange={() => calculateNormalized(course[0])}
+                                >
+                                    <option value="A" selected={course[4] === "A"}>
+                                        A
+                                    </option>
+                                    <option value="B" selected={course[4] === "B"}>
+                                        B
+                                    </option>
+                                    <option value="C" selected={course[4] === "C"}>
+                                        C
+                                    </option>
+                                    <option value="D" selected={course[4] === "D"}>
+                                        D
+                                    </option>
+                                    <option value="E" selected={course[4] === "E"}>
+                                        E
+                                    </option>
+                                    <option value="F" selected={course[4] === "F"}>
+                                        F
+                                    </option>
+                                    <option value="Bestått" selected={course[4] === "Bestått"}>
+                                        Bestått
+                                    </option>
+                                    <option
+                                        value="Ikke bestått"
+                                        selected={course[4] === "Ikke bestått"}
+                                    >
+                                        Ikke bestått
+                                    </option>
+                                </select>
+                            </td>
+                            <td id={`normalized-${course[0]}`}>
+                                {characterToNumber(course[4]) * parseFloat(course[3])}
+                            </td>
+                        </tr>
+                    ))}
+                </table>
             </header>
         </div>
     );
-}
-
-function setData(data) {
-    // convert finalResult to a table and add it to the DOM
-    const table = document.getElementById("table");
-    table.innerHTML = `
-            <tr>
-                <th>Emnekode</th>
-                <th></th>
-                <th>Termin</th>
-                <th>Vekt</th>
-                <th>Karakter</th>
-                <th>Normalisert</th>
-            </tr>
-        `;
-    data.forEach(course => {
-            table.innerHTML += `
-                <tr>
-                    <td>${course[0]}</td>
-                    <td>${course[1]}</td>
-                    <td>${course[2]}</td>
-                    <td>${parseFloat(course[3])}</td>
-                    <td>${course[4]}</td>
-                    <td>${characterToNumber(course[4]) * parseFloat(course[3])}</td>
-                </tr>
-            `;
-        }
-    );
-    //console.log(finalResult);
-    //document.body.appendChild(table);
-
-    let averageSpan = document.getElementById("average");
-    let sum = 0;
-    let points = 0;
-    data.forEach(course => {
-            if (course[4] !== "Bestått") {
-                sum += characterToNumber(course[4]) * parseFloat(course[3]);
-                points += parseFloat(course[3]);
-            }
-        }
-    );
-    averageSpan.innerText = (sum / points).toFixed(2);
-
-    let totalPointsSpan = document.getElementById("totalPoints");
-    totalPointsSpan.innerText = points.toFixed(2) + "stp";
-}
-
-function characterToNumber(character) {
-    if (character === "A") {
-        return 5.0;
-    }
-    else if (character === "B") {
-        return 4.0;
-    }
-    else if (character === "C") {
-        return 3.0;
-    }
-    else if (character === "D") {
-        return 2.0;
-    }
-    else if (character === "E") {
-        return 1.0;
-    }
-    else {
-        return 0;
-    }
 }
 
 export default App;
--- src/DataParser.js
+++ src/DataParser.js
@@ -1,93 +1,97 @@
 import { getDocument } from 'pdfjs-dist/webpack';
 
-export function parseData(event) {
-    // if no file is selected, send an alert
-    if (document.getElementById('myFile').files.length === 0) {
-        alert('Vennligst velg et fil');
-        return;
-    }
+export async function parseData(event) {
     event.preventDefault();
 
-    return new Promise((resolve, reject) => {
-        const file = document.getElementById('myFile').files[0];
-        const reader = new FileReader();
+    const fileInput = document.getElementById('myFile');
+    if (fileInput.files.length === 0) {
+        alert('Vennligst velg en fil');
+        return;
+    }
 
-        reader.onload = async function (e) {
-            const data = new Uint8Array(e.target.result);
+    const file = fileInput.files[0];
+    const data = await readFile(file);
+    const text = await extractText(data);
+    const finalResult = processText(text);
+
+    // remove last element in the array
+    finalResult.pop();
+
+    return finalResult;
+}
+
+function readFile(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (e) => resolve(new Uint8Array(e.target.result));
+        reader.onerror = (e) => reject(e);
+        reader.readAsArrayBuffer(file);
+    });
+}
+
+function extractText(data) {
+    return new Promise(async (resolve, reject) => {
+        try {
             const pdf = await getDocument(data).promise;
             const page = await pdf.getPage(1);
             const textContent = await page.getTextContent();
             const text = textContent.items.map((item) => item.str).join(' ');
-
-            const textArea = document.createElement('textarea');
-            textArea.value = text;
-            //console.log(text);
-
-            // keep everything after " A B C D E "
-            let finalResult = [];
-
-            try {
-                let grades = text.split(' A B C D E ')[1];
-
-                // replace "Bestått" with "X"
-                grades = grades.replace(/Bestått/g, 'X');
-
-                // Split on capital single letter words and "Bestått" but don't remove them
-                const result = grades.split(/ ([A-Z]) /).filter(Boolean);
-
-                const combinedResult = [];
-                for (let i = 0; i < result.length; i += 2) {
-                    // remove "underfined" if some words contain "underfined"
-                    if (result[i + 1] === undefined) {
-                        result[i + 1] = '';
-                    }
-
-                    combinedResult.push(result[i] + result[i + 1]);
-                }
-
-                //remove last element
-                combinedResult.pop();
-
-                // print courses
-                combinedResult.forEach((course) => {
-                    // split on more than one space
-                    const courseInfo = course.split(/  +/);
-
-                    // if courseInfo[2] doesn't contain a number, then join it to the previous element
-                    if (!courseInfo[2].match(/\d+/)) {
-                        courseInfo[1] += ' - ' + courseInfo[2];
-                        courseInfo.splice(2, 1);
-                    }
-
-                    // if third element contains " stp" then remove it
-                    if (courseInfo[3].includes(' stp')) {
-                        courseInfo[3] = courseInfo[3].replace(' stp', '');
-                    }
-
-                    courseInfo[0] = courseInfo[0].trim();
-
-                    courseInfo[3] = courseInfo[3].replace(',', '.');
-
-                    // if last element contains "X" then remove it from combinedResult
-                    //if (courseInfo[courseInfo.length - 1] !== ("X")) {
-
-                    if (isNaN(courseInfo[3])) {
-                        courseInfo[3] = '0';
-                    }
-
-                    if (courseInfo[4] === 'X') {
-                        courseInfo[4] = 'Bestått';
-                    }
-
-                    finalResult.push(courseInfo);
-                    //}
-                });
-
-                resolve(finalResult);
-            } catch (error) {
-                reject('Vennligst velg et vitnemål fra vitnemalsportalen');
-            }
-        };
-        reader.readAsArrayBuffer(file);
+            resolve(text);
+        } catch (error) {
+            reject('Vennligst velg et vitnemål fra vitnemalsportalen');
+        }
     });
+}
+
+function processText(text) {
+    const grades = extractGrades(text);
+    const processedGrades = replaceBestatt(grades);
+    const combinedResult = combineGrades(processedGrades);
+    const finalResult = formatCourses(combinedResult);
+    return finalResult;
+}
+
+function extractGrades(text) {
+    const marker = ' A B C D E ';
+    const startIndex = text.indexOf(marker) + marker.length;
+    const grades = text.substr(startIndex);
+    return grades;
+}
+
+function replaceBestatt(grades) {
+    return grades.replace(/Bestått/g, 'X');
+}
+
+function combineGrades(grades) {
+    const result = grades.split(/ ([A-Z]) /).filter(Boolean);
+    const combinedResult = [];
+    for (let i = 0; i < result.length; i += 2) {
+        const currentGrade = result[i] + result[i + 1];
+        combinedResult.push(currentGrade);
+    }
+    return combinedResult;
+}
+
+function formatCourses(combinedResult) {
+    const finalResult = [];
+    combinedResult.forEach((course) => {
+        const courseInfo = course.split(/  +/);
+        if (!courseInfo[2].match(/\d+/)) {
+            courseInfo[1] += ' - ' + courseInfo[2];
+            courseInfo.splice(2, 1);
+        }
+        if (courseInfo[3].includes(' stp')) {
+            courseInfo[3] = courseInfo[3].replace(' stp', '');
+        }
+        courseInfo[0] = courseInfo[0].trim();
+        courseInfo[3] = courseInfo[3].replace(',', '.');
+        if (isNaN(courseInfo[3])) {
+            courseInfo[3] = '0';
+        }
+        if (courseInfo[4] === 'X') {
+            courseInfo[4] = 'Bestått';
+        }
+        finalResult.push(courseInfo);
+    });
+    return finalResult;
 }
